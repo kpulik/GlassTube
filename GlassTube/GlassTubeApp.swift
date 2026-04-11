0a22d3c7979955d682b8285663a409386d6cfff7
k_pulik <63879539+kpulik@users.noreply.github.com>
2026-04-11 18:21:46 -0500

    Initial Commit

---
+++ GlassTube/GlassTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassTubeApp.swift
+//  GlassTube
+//
+//  Created by Kevin Pulikkottil on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct GlassTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
